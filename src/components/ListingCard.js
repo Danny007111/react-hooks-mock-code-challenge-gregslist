14d988683e59bd53fb8a4ba59afe5eb3129094a8
Danny Rodriguez <danny007111@hotmail.com>
2022-06-22 16:51:24 -0500

    Completed assignment

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({listingItem, onDeleteListing}) {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  function handleDelete() {
+    fetch(`http://localhost:6001/listings/${listingItem.id}`, {
+      method: "DELETE",
+    })
+      .then((u) => u.json())
+      .then(() => {
+        onDeleteListing(listingItem.id);
+      });
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listingItem.image} alt={listingItem.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFavorited ? (
+          <button onClick={() => setIsFavorited(false)} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={() => setIsFavorited(true)} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listingItem.description}</strong>
+        <span> · {listingItem.location}</span>
+        <button onClick={handleDelete} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,43 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([]);
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((u) => u.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function handleDeleteListing(id) {
+    const updatedListingsArray = listings.filter((listing) => 
+      listing.id !== id
+    );
+    setListings(updatedListingsArray);
+  };
+
+  const filteredListings = listings.filter((listing) => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+  
+  console.log(filteredListings)
+
+  const listingCards = filteredListings.map((listing) => (
+      
+      <ListingCard
+        key={listing.id}
+        listingItem={listing}
+        onDeleteListing={handleDeleteListing}
+      />
+      
+    ));
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
